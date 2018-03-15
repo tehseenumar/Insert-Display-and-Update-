20d36c8d69b3951fd41e9511359b515e0cab72e8
Tehseen Umar <37043205+tehseenumar@users.noreply.github.com>
2018-03-15 19:15:43 +0500

    Update insert.php

--- insert.php
+++ insert.php
@@ -38,39 +38,6 @@
 
 
 
-		/*
-		// Image Veriable Dec
-		$filename = addslashes($_FILES["img"]["name"]);
-	    $tmpname = addslashes(file_get_contents($_FILES["img"]["tmp_name"]));
-	    $filetype = addslashes($_FILES["img"]["type"]);
-	    $array = array('jpg','jpeg','png');
-	    $ext = pathinfo($filename, PATHINFO_EXTENSION);	
-
-
-	    // Query of file upload
-
-	    if($filename!= "" && $rn!= "" && $sn!= "" && $cl!= ""){
-	        // if(in_array($ext, $array)){
-	            //$query = "INSERT INTO image (name, image) VALUES('$filename', '$tmpname')";
-	            $query = "INSERT INTO student (rollno, name, class, image, img_name) VALUES('$rn','$sn','$cl','$filename','$tmpname')";
-	            $data = mysqli_query($conn, $query);
-	            if($data){
-					echo "Data Insert into Database Successfully. <a href='display.php'>Check Updated List Here</a>";
-				}else{
-					echo "Already Data Insert Database";
-				}
-	         //}else{
-	           // echo "Unsupported format.";
-	        // }
-      }else{
-         echo "All Fields are Required.";
-      }
-
-*/
-
-
-
-
 		if($rn!= "" && $sn!= "" && $cl!= "" && $file!= ""){
 				$query = "INSERT INTO student (rollno, name, class, image) VALUES('$rn','$sn','$cl', '$file')";
 				$data = mysqli_query($conn, $query);
